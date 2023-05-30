e3a09dc168ad44617310c8b4b3042433b45ba9a3
yvandouenel <y.douenel@coopernet.fr>
2023-05-30 11:36:19 +0200

    First commit après la création des components home et HousingLocationComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
