37924ac130452ba8c9038588c6e28ee80234fb8a
yvandouenel <y.douenel@coopernet.fr>
2023-06-01 12:16:08 +0200

    Ajout des routes vers les détails d'une location.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component,NgModule } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  standalone: true,
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, RouterModule],
 })
 export class AppComponent {
   title = 'my-first-project';
 }
-
