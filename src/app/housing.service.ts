7f7d0089103fa4cbd5a1aa34a41858038a41a63c
yvandouenel <y.douenel@coopernet.fr>
2023-06-06 16:44:31 +0200

    ajout des données via json-server et utilisation des promesses.

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,50 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housinglocation';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/example-house1.jpg',
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: '/assets/example-house2.jpg',
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: '/assets/example-house3.jpg',
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    },
-  ];
-  constructor() { }
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+
+  url = 'http://localhost:3000/locationsss';
+
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
 
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
+
   submitApplication(firstName: string, lastName: string, email: string) {
-    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+    console.log(firstName, lastName, email);
   }
 }
