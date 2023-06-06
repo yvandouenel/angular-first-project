23c0f254dee2d859413fed03bcf4b83ecaa59f56
Yvan Douënel <y.douenel@coopernet.fr>
2023-06-06 16:03:07 +0200

    ajout de services de récupération des données via json-server.

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -40,8 +40,11 @@ export class HousingService {
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
-  
+
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
 }
